dac6289bd56da2a131013289ab84bb4cb6844463
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-09-10 16:31:49 +0530

    CRUD added

--- Django_CRM/website/models.py
+++ Django_CRM/website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
+    zipcode = models.CharField(max_length=15)
+
+
+    def __str__(self) -> str:
+        return (f"{self.first_name} {self.last_name}")
+    
